a85e2332472aa122a975cb1dac2fe680b34a1092
Martijn Visser <mgvisser@gmail.com>
2019-09-19 22:53:32 +0200

    switch to iterating over Row object, much faster

--- src/DBFTables.jl
+++ src/DBFTables.jl
@@ -4,132 +4,164 @@ using Printf, Tables, WeakRefStrings
 
 "Field/column descriptor, part of the Header"
 struct FieldDescriptor
-	name::Symbol
-	type::DataType
-	length::UInt8
-	ndec::UInt8
+    name::Symbol
+    type::DataType
+    length::UInt8
+    ndec::UInt8
 end
 
 "DBF header, which also holds all field definitions"
 struct Header
-	version::UInt8
-	last_update::String
-	records::UInt32
-	hsize::UInt16
-	rsize::UInt16
-	incomplete::Bool
-	encrypted::Bool
-	mdx::Bool
-	lang_id::UInt8
-	fields::Vector{FieldDescriptor}
+    version::UInt8
+    last_update::String
+    records::UInt32
+    hsize::UInt16
+    rsize::UInt16
+    incomplete::Bool
+    encrypted::Bool
+    mdx::Bool
+    lang_id::UInt8
+    fields::Vector{FieldDescriptor}
+end
+
+"Struct representing the DBF Table"
+struct Table
+    header::Header
+    data::Vector{UInt8}  # WeakRefString references this
+    strings::StringArray{WeakRefString{UInt8},2}
+end
+
+"Struct representing a single row or record of the DBF Table"
+struct Row
+    table::Table
+    row::Int
 end
 
 "Convert DBF data type characters to Julia types"
 function typemap(fld::Char, ndec::UInt8)
-	# https://www.clicketyclick.dk/databases/xbase/format/data_types.html
-	rt = Nothing
-	if fld == 'C'
-		rt = String
-	elseif fld == 'D'
-		rt = String
-	elseif fld == 'N'
-		if ndec > 0
-			rt = Float64
-		else
-			# TODO do we want this?
-			rt = Int
-		end
-	elseif fld == 'F' || fld == 'O'
-		rt = Float64
-	elseif fld == 'I' || fld == '+'
-		rt = Int
-	elseif fld == 'L'
-		rt = Bool
-	else
-		throw(ArgumentError("Unknown record type $fld"))
-	end
-	return rt
+    # https://www.clicketyclick.dk/databases/xbase/format/data_types.html
+    rt = Nothing
+    if fld == 'C'
+        rt = String
+    elseif fld == 'D'
+        rt = String
+    elseif fld == 'N'
+        if ndec > 0
+            rt = Float64
+        else
+            # TODO do we want this?
+            rt = Int
+        end
+    elseif fld == 'F' || fld == 'O'
+        rt = Float64
+    elseif fld == 'I' || fld == '+'
+        rt = Int
+    elseif fld == 'L'
+        rt = Bool
+    else
+        throw(ArgumentError("Unknown record type $fld"))
+    end
+    return rt
 end
 
 "Read a field descriptor from the stream, and create a FieldDescriptor struct"
 function read_dbf_field(io::IO)
-	field_name_raw = String(read!(io, Vector{UInt8}(undef, 11)))
-	field_name = Symbol(strip(replace(field_name_raw, '\0'=>' ')))
-	field_type = read(io, Char)
-	skip(io, 4)  # skip
-	field_len = read(io, UInt8)
-	field_dec = read(io, UInt8)
-	skip(io, 14)  # reserved
-	jltype = typemap(field_type, field_dec)
-	return FieldDescriptor(field_name, jltype, field_len, field_dec)
+    field_name_raw = String(read!(io, Vector{UInt8}(undef, 11)))
+    field_name = Symbol(strip(replace(field_name_raw, '\0' => ' ')))
+    field_type = read(io, Char)
+    skip(io, 4)  # skip
+    field_len = read(io, UInt8)
+    field_dec = read(io, UInt8)
+    skip(io, 14)  # reserved
+    jltype = typemap(field_type, field_dec)
+    return FieldDescriptor(field_name, jltype, field_len, field_dec)
 end
 
 "Read a DBF header from a stream"
 function Header(io::IO)
-	ver = read(io, UInt8)
-	date1 = read(io, UInt8)
-	date2 = read(io, UInt8)
-	date3 = read(io, UInt8)
-	last_update = @sprintf("%4d%02d%02d", date1+1900, date2, date3)
-	records = read(io, UInt32)
-	hsize = read(io, UInt16)
-	rsize = read(io, UInt16)
-	skip(io, 2)  # reserved
-	incomplete = Bool(read(io, UInt8))
-	encrypted = Bool(read(io, UInt8))
-	skip(io, 12)  # reserved
-	mdx = Bool(read(io, UInt8))
-	langId = read(io, UInt8)
-	skip(io, 2)  # reserved
-	fields = FieldDescriptor[]
+    ver = read(io, UInt8)
+    date1 = read(io, UInt8)
+    date2 = read(io, UInt8)
+    date3 = read(io, UInt8)
+    last_update = @sprintf("%4d%02d%02d", date1 + 1900, date2, date3)
+    records = read(io, UInt32)
+    hsize = read(io, UInt16)
+    rsize = read(io, UInt16)
+    skip(io, 2)  # reserved
+    incomplete = Bool(read(io, UInt8))
+    encrypted = Bool(read(io, UInt8))
+    skip(io, 12)  # reserved
+    mdx = Bool(read(io, UInt8))
+    lang_id = read(io, UInt8)
+    skip(io, 2)  # reserved
+    fields = FieldDescriptor[]
 
-	while !eof(io)
-		push!(fields, read_dbf_field(io))
-		mark(io)
-		trm = read(io, UInt8)
-		if trm == 0xD
-			break
-		else
-			reset(io)
-		end
-	end
+    while !eof(io)
+        push!(fields, read_dbf_field(io))
+        mark(io)
+        trm = read(io, UInt8)
+        if trm == 0xD
+            break
+        else
+            reset(io)
+        end
+    end
 
-	return Header(ver, last_update, records, hsize, rsize,
-				  incomplete, encrypted, mdx, langId, fields)
+    return Header(
+        ver,
+        last_update,
+        records,
+        hsize,
+        rsize,
+        incomplete,
+        encrypted,
+        mdx,
+        lang_id,
+        fields,
+    )
 end
 
 miss(x) = ifelse(x === nothing, missing, x)
 
 "Concert a DBF entry string to a Julia value"
 function dbf_value(T::Type{Bool}, str::AbstractString)
-	char = first(str)
-	if char in "YyTt"
-		true
-	elseif char in "NnFf"
-		false
-	elseif char == '?'
-		missing
-	else
-		throw(ArgumentError("Unknown logical $char"))
-	end
+    char = first(str)
+    if char in "YyTt"
+        true
+    elseif char in "NnFf"
+        false
+    elseif char == '?'
+        missing
+    else
+        throw(ArgumentError("Unknown logical $char"))
+    end
 end
 
-dbf_value(T::Union{Type{Int}, Type{Float64}}, str::AbstractString) = miss(tryparse(T, str))
+dbf_value(T::Union{Type{Int},Type{Float64}}, str::AbstractString) =
+    miss(tryparse(T, str))
 # String to avoid returning SubString{WeakRefString{UInt8}}
-dbf_value(T::Type{String}, str::AbstractString) = String(rstrip(str))
+function dbf_value(T::Type{String}, str::AbstractString)
+    stripped = rstrip(str)
+    if isempty(stripped)
+        # return missing rather than ""
+        return missing
+    else
+        return String(stripped)
+    end
+end
 dbf_value(T::Type{Nothing}, str::AbstractString) = missing
 
-"DBF"
-struct Table
-	header::Header
-	data::Vector{UInt8}  # WeakRefString reference this
-	strings::StringArray{WeakRefString{UInt8}, 2}
-end
-
+# define get functions using getfield since we overload getproperty
 "Access the header of a DBF Table"
-header(dbf::Table) = getfield(dbf, :header)
-fields(dbf::Table) = header(dbf).fields
-strings(dbf::Table) = getfield(dbf, :strings)
+getheader(dbf::Table) = getfield(dbf, :header)
+getfields(dbf::Table) = getheader(dbf).fields
+getstrings(dbf::Table) = getfield(dbf, :strings)
+getrow(row::Row) = getfield(row, :row)
+gettable(row::Row) = getfield(row, :table)
+
+Base.length(dbf::Table) = Int(getheader(dbf).records)
+Base.size(dbf::Table) = (length(dbf), length(getfields(dbf)))
+Base.size(dbf::Table, i) = size(dbf)[i]
 
 """
 	DBFTables.Table(source) => DBFTables.Table
@@ -140,82 +172,93 @@ to other formats. It is possible to iterate through the rows of this object,
 or to retrieve columns like `dbf.fieldname`.
 """
 function Table(io::IO)
-	header = Header(io)
-	# consider using mmap here for big dbf files
-	data = Vector{UInt8}(undef, header.rsize * header.records)
-	read!(io, data)
-	strings = _create_stringarray(header, data)
-	Table(header, data, strings)
+    header = Header(io)
+    # consider using mmap here for big dbf files
+    data = Vector{UInt8}(undef, header.rsize * header.records)
+    read!(io, data)
+    strings = _create_stringarray(header, data)
+    Table(header, data, strings)
 end
 
 function Table(path::AbstractString)
-	open(path) do io
-		Table(io)
-	end
+    open(path) do io
+        Table(io)
+    end
 end
 
 "Collect all the offsets and lenghts from the header to create a StringArray"
 function _create_stringarray(header::Header, data::AbstractVector)
-	# first make the lengths and offsets for a single record
-	lengths_record = UInt32.(getfield.(header.fields, :length))
-	offsets_record = vcat(0, cumsum(lengths_record)[1:end-1]) .+ 1
+    # first make the lengths and offsets for a single record
+    lengths_record = UInt32.(getfield.(header.fields, :length))
+    offsets_record = vcat(0, cumsum(lengths_record)[1:end-1]) .+ 1
 
-	# the lengths are equal for each record
-	lengths = repeat(lengths_record, 1, header.records)
-	# the offsets accumulate over records with the record size
-	row_offsets = range(0; length=header.records, step=header.rsize)
-	offsets = repeat(offsets_record, 1, header.records)
-	offsets .+= reshape(row_offsets, 1, :)
+    # the lengths are equal for each record
+    lengths = repeat(lengths_record, 1, header.records)
+    # the offsets accumulate over records with the record size
+    row_offsets = range(0; length = header.records, step = header.rsize)
+    offsets = repeat(offsets_record, 1, header.records)
+    offsets .+= reshape(row_offsets, 1, :)
 
-	StringArray{WeakRefString{UInt8}, 2}(data, offsets, lengths)
+    StringArray{WeakRefString{UInt8},2}(data, offsets, lengths)
 end
 
 "Create a NamedTuple representing a single row"
-function _create_namedtuple(dbf::Table, row::Integer)
-    ncol = length(fields(dbf))
-	sch = Tables.schema(dbf)
-	record = strings(dbf)[:, row]
-	# convert typle of types to a tuple type, needed for NamedTuple
-	ttypes = Tuple{sch.types...}
-    NamedTuple{sch.names, ttypes}(
-        (dbf_value(fields(dbf)[col].type, record[col]) for col in 1:ncol)
-    )
+function Base.NamedTuple(row::Row)
+    dbf = gettable(row)
+    str = getstrings(dbf)
+    fields = getfields(dbf)
+    ncol = length(fields)
+    rowidx = getrow(row)
+    @inbounds record = @view str[:, rowidx]
+    @inbounds prs = (fields[col].name => dbf_value(
+        fields[col].type,
+        record[col],
+    ) for col = 1:ncol)
+    return (; prs...)
 end
 
-Base.isempty(dbf::Table) = header(dbf).records == 0
+function Base.show(io::IO, row::Row)
+    show(io, NamedTuple(row))
+end
+
+function Base.show(io::IO, dbf::Table)
+    nr = length(dbf)
+    nc = length(getfields(dbf))
+    println(io, "DBFTables.Table with $nr rows and $nc columns")
+end
+
+Base.isempty(dbf::Table) = getheader(dbf).records == 0
 
 "Get a BitVector which is true for rows that are marked as deleted"
 function isdeleted(dbf::Table)
-	data = getfield(dbf, :data)
-	rsize = header(dbf).rsize
-	nrow = header(dbf).records
-	idx = range(1, step=rsize, length=nrow)
-	data[idx] .== 0x2a
+    data = getfield(dbf, :data)
+    rsize = getheader(dbf).rsize
+    nrow = getheader(dbf).records
+    idx = range(1, step = rsize, length = nrow)
+    data[idx] .== 0x2a
 end
 
 "Check if the row is marked as deleted"
 function isdeleted(dbf::Table, row::Integer)
-	data = getfield(dbf, :data)
-	i = (row - 1) * header(dbf).rsize + 1
-	data[i] == 0x2a
+    data = getfield(dbf, :data)
+    i = (row - 1) * getheader(dbf).rsize + 1
+    data[i] == 0x2a
 end
-
-
-### the functions below implement the Tables interface
 
 "Iterate over the rows of a DBF Table, yielding a NamedTuple for each row"
-function Base.iterate(dbf::Table)
-    isempty(dbf) && return nothing
-	data = getfield(dbf, :data)
-	data[1, 1] == 0x2a
-    nt = DBFTables._create_namedtuple(dbf, 1)
-    return nt, 2
+function Base.iterate(dbf::Table, st = 1)
+    st > length(dbf) && return nothing
+    return Row(dbf, st), st + 1
 end
 
-function Base.iterate(dbf::Table, st)
-    st > header(dbf).records && return nothing
-    nt = DBFTables._create_namedtuple(dbf, st)
-    return nt, st + 1
+function Base.getproperty(row::Row, name::Symbol)
+    dbf = gettable(row)
+    str = getstrings(dbf)
+    colidx = findfirst(x -> x === name, propertynames(dbf))
+    colidx === nothing && throw(ArgumentError("Column not present: $name"))
+    type = @inbounds getfields(dbf)[colidx].type
+    rowidx = getrow(row)
+    return @inbounds dbf_value(type, str[colidx, rowidx])
 end
 
 Tables.istable(::Type{Table}) = true
@@ -226,22 +269,24 @@ Tables.columns(dbf::Table) = dbf
 
 "Get the Tables.Schema of a DBF Table"
 function Tables.schema(dbf::Table)
-	names = Tuple(field.name for field in fields(dbf))
-	# since missing is always supported, add it to the schema types
-	types = Tuple(Union{field.type, Missing} for field in fields(dbf))
-	Tables.Schema(names, types)
+    names = Tuple(field.name for field in getfields(dbf))
+    # since missing is always supported, add it to the schema types
+    types = Tuple(Union{field.type,Missing} for field in getfields(dbf))
+    Tables.Schema(names, types)
 end
 
 "List all available DBF column names"
 Base.propertynames(dbf::Table) = getfield.(getfield(dbf, :header).fields, :name)
 
 "Get an entire DBF column as a Vector. Usage: `dbf.myfield`"
-function Base.getproperty(dbf::Table, nm::Symbol)
-    col = findfirst(x -> x === nm, propertynames(dbf))
-	nrow = header(dbf).records
-	type = fields(dbf)[col].type
-	str = strings(dbf)
-	[dbf_value(type, str[col, i]) for i = 1:nrow]
+function Base.getproperty(dbf::Table, name::Symbol)
+    col = findfirst(x -> x === name, propertynames(dbf))
+    col === nothing && throw(ArgumentError("Column not present: $name"))
+    nrow = getheader(dbf).records
+    @inbounds type = getfields(dbf)[col].type
+    str = getstrings(dbf)
+    @inbounds colarr = [dbf_value(type, str[col, i]) for i = 1:nrow]
+    return colarr
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,94 +1,78 @@
 using DBFTables
 using Test
 using Tables
-using WeakRefStrings
 using DataFrames
 
 test_dbf_path = joinpath(@__DIR__, "test.dbf")
-dbf = DBFTables.Table(open(test_dbf_path))
+dbf = DBFTables.Table(test_dbf_path)
 df = DataFrame(dbf)
-
-@testset "DataFrame indexing" begin
-    @test size(df, 1) == 7 # records
-    @test size(df, 2) == 6 # fields
-    @test df[2, :CHAR] == "John"
-    @test df[1, :DATE] == "19900102"
-    @test df[3, :BOOL] == false
-    @test df[1, :FLOAT] == 10.21
-    @test df[2, :NUMERIC] == 12.21
-    @test df[3, :INTEGER] == 102
-end
-
-@testset "missing entries" begin
-    @test ismissing(df[4, :BOOL])
-    @test ismissing(df[5, :FLOAT])
-    @test ismissing(df[6, :NUMERIC])
-    @test ismissing(df[7, :INTEGER])
-end
-
-@testset "header" begin
-    h = DBFTables.Header(open(test_dbf_path))
-    @test h.version == 3
-    @test h.lastUpdate == "20140806"
-    @test h.records == 7
-    @test length(h.fields) == 6
-end
-
 row, st = iterate(dbf)
-@test st == 2
-row
 
+@testset "DBFTables" begin
+    @testset "DataFrame indexing" begin
+        @test size(df, 1) == 7 # records
+        @test size(df, 2) == 6 # fields
+        @test df[2, :CHAR] == "John"
+        @test df[1, :DATE] == "19900102"
+        @test df[3, :BOOL] == false
+        @test df[1, :FLOAT] == 10.21
+        @test df[2, :NUMERIC] == 12.21
+        @test df[3, :INTEGER] == 102
+    end
 
+    @testset "missing entries" begin
+        @test ismissing(df[4, :BOOL])
+        @test ismissing(df[5, :FLOAT])
+        @test ismissing(df[6, :NUMERIC])
+        @test ismissing(df[7, :INTEGER])
+    end
 
+    @testset "header" begin
+        h = DBFTables.Header(open(test_dbf_path))
+        @test h.version == 3
+        @test h.last_update == "20140806"
+        @test h.records == 7
+        @test length(h.fields) == 6
+    end
 
+    @testset "show" begin
+        @test sprint(show, row) === sprint(show, NamedTuple(row))
+        @test sprint(
+            show,
+            dbf,
+        ) === "DBFTables.Table with 7 rows and 6 columns\n"
+    end
 
+    @testset "iterate" begin
+        @test st === 2
+        @test row.CHAR === "Bob"
+        firstrow = (
+            CHAR = "Bob",
+            DATE = "19900102",
+            BOOL = false,
+            FLOAT = 10.21,
+            NUMERIC = 11.21,
+            INTEGER = 100,
+        )
+        @test NamedTuple(row) === firstrow
+        @test row isa DBFTables.Row
+        @test DBFTables.getrow(row) === 1
+        @test DBFTables.gettable(row) === dbf
+        @test sum(1 for row in dbf) === 7
+    end
 
+    @testset "column" begin
+        @test size(dbf) === (7, 6)
+        @test size(dbf, 2) === 6
 
+        @test length(dbf.CHAR) === 7
+        @test dbf.CHAR isa Vector{Union{String,Missing}}
+        @test dbf.INTEGER isa Vector{Union{Int,Missing}}
+        @test dbf.INTEGER[2] === 101
+        @test ismissing(dbf.INTEGER[7])
+        @test dbf.CHAR[2] === "John"
+        @test ismissing(dbf.CHAR[7])
 
-chkrow = NamedTuple{
-    (:CHAR, :DATE, :BOOL, :FLOAT, :NUMERIC, :INTEGER),
-    Tuple{Union{Missing, String},Union{Missing, String},Union{Missing, Bool},
-    Union{Missing, Float64},Union{Missing, Float64},Union{Missing, Int64}}}(
-        ("Bob", "19900102", false, 10.21, 11.21, 100))
-row === chkrow
-row.CHAR
-row == (CHAR=2, DATE="19000102", BOOL=false, FLOAT=10.21, NUMERIC=11.21, INTEGER=100)
-# test a few other files
-ne_path = "c:/tmp/ne/ne_10m_admin_0_boundary_lines_land/ne_10m_admin_0_boundary_lines_land.dbf"
+    end
 
-using BenchmarkTools
-# 367.300 μs (250 allocations: 489.03 KiB)
-@btime DBFTables.Table($ne_path)
-t = DBFTables.Table(ne_path)
-r, s = iterate(t)
-r.scalerank
-unique(t.scalerank)
-t.scalerank
-# 1.319 s (219477 allocations: 12.24 MiB)
-@btime sum(row.scalerank for row in $t)
-# 69.800 μs (475 allocations: 18.69 KiB)
-@btime sum($(t).scalerank)
-
-
-
-t = DBFTables.Table(open(ne_path))
-getfield(t, :data)
-DBFTables.isdeleted(t)
-
-#=
-org_path = "c:/tmp/ne/shp_deleted/vector.dbf"
-h = DBFTables.Header(open(org_path))
-Int(h.records)
-t = DBFTables.Table(open(org_path))
-DBFTables.isdeleted(t)
-count(DBFTables.isdeleted(t))
-DataFrame(t)
-
-org_path = "c:/tmp/ne/shp_deleted/vector_edited.dbf"
-h = DBFTables.Header(open(org_path))
-Int(h.records)
-t = DBFTables.Table(open(org_path))
-DBFTables.isdeleted(t)
-count(DBFTables.isdeleted(t))
-DataFrame(t)
-=#
+end  # testset "DBFTables"
